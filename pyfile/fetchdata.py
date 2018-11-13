82aa9ae594a36190d48eef71618c6260b3b458bd
sijiawang <sijiawang@wustl.edu>
2018-11-13 11:33:45 -0600

    test

---
+++ pyfile/fetchdata.py
@@ -0,0 +1,8 @@
+from get_nba_data import advanced_stats
+
+advanced = advanced_stats()
+fatties = advanced.get_data(college="duke", weight=">250")
+sticks = advanced.get_data(college="duke", weight="<200")
+advanced = advanced_stats()
+fatties = advanced.get_data(college="duke", weight=">250")
+sticks = advanced.get_data(college="duke", weight="<200")
